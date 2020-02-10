31f9ad2d9c32fc214c5c0642b20d92be752c6b5c
parthsardhara <parth.sardhara@bacancytechnology.com>
2020-02-10 11:23:11 +0530

    implementation of interceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoaderService } from './loader.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interceptor';
+  title = 'Interceptor';
+  public loading = false;
+
+
+  public items: any = [];
+
+  constructor(private http: HttpClient,
+    private loaderService: LoaderService,
+  ) { }
+
+  ngOnInit(): void {
+    this.loaderService
+    .getSpinnerObs().subscribe(val => {
+      this.loading = val;
+    })
+  }
+  public clickAPI() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((result) => {
+      console.log('res', result);
+      this.items = result;
+    });
+  }
+
+  public clear() {
+    this.items = [];
+  }
 }
